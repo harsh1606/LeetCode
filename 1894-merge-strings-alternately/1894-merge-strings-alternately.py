f857d012d9e2394f691fc277049b44bf40ef67a4
Harsh Kumar Tyagi <48941096+harsh1606@users.noreply.github.com>
2025-03-14 04:00:31 +0530

    Time: 45 ms (13.37%), Space: 17.6 MB (73.78%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -14,12 +14,23 @@ class Solution:
         #             s = s + word1[i:]
         #         break
         # return s
-        min_len = min(len(word1),len(word2))
-        for i in range(min_len):
-            s = s + word1[i] + word2[i]
+        # min_len = min(len(word1),len(word2))
+        # for i in range(min_len):
+        #     s = s + word1[i] + word2[i]
     
-        if len(word1) > len(word2):
-            s += word1[i+1:]
-        else:
-            s += word2[i+1:]
+        # if len(word1) > len(word2):
+        #     s += word1[i+1:]
+        # else:
+        #     s += word2[i+1:]
+        # return s
+
+        i = j = 0
+        s = ''
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                s += word1[i]
+                i += 1
+            if j < len(word2):
+                s += word2[j]
+                j += 1
         return s
