99d6a9c31eaa1e82d0f83084612218149f33c4af
Harsh Kumar Tyagi <48941096+harsh1606@users.noreply.github.com>
2025-07-15 18:57:15 +0530

    Time: 308 ms (56.01%), Space: 0B (100%) - LeetHub

--- 3848-analyze-subscription-conversion/3848-analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/3848-analyze-subscription-conversion.sql
@@ -2,7 +2,8 @@ select user_id ,
 round(avg(case when activity_type = 'free_trial' then activity_duration end), 2) as trial_avg_duration,
 round(avg(case when activity_type = 'paid' then activity_duration end), 2) as paid_avg_duration  
 from UserActivity 
-where user_id in (select distinct user_id
-                    from UserActivity
-                    where activity_type = 'paid')
+-- where user_id in (select distinct user_id
+--                     from UserActivity
+--                     where activity_type = 'paid')
 group by user_id 
+having Avg(CASE WHEN activity_type = 'paid' THEN activity_duration END) IS NOT NULL
