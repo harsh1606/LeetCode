b64bcc0221c9051c6524f14c7ded574f6bf04e31
Harsh Kumar Tyagi <48941096+harsh1606@users.noreply.github.com>
2025-07-15 18:56:14 +0530

    Time: 325 ms (33.93%), Space: 0B (100%) - LeetHub

---
+++ 3848-analyze-subscription-conversion/3848-analyze-subscription-conversion.sql
@@ -0,0 +1,8 @@
+select user_id , 
+round(avg(case when activity_type = 'free_trial' then activity_duration end), 2) as trial_avg_duration,
+round(avg(case when activity_type = 'paid' then activity_duration end), 2) as paid_avg_duration  
+from UserActivity 
+where user_id in (select distinct user_id
+                    from UserActivity
+                    where activity_type = 'paid')
+group by user_id 
